53a961071f28fbf41588c7c5fb34bd5b8b3207bf
dkulik <kulikdenis1996x@gmail.com>
2026-03-08 18:42:43 +0700

    feat: add file logger

--- src/middlewares/logger-middleware.ts
+++ src/middlewares/logger-middleware.ts
@@ -1,21 +1,13 @@
 import { Request, Response, NextFunction } from "express";
+import { writeLog } from "../utils/file-logger";
 
 export const loggerMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const start = Date.now();
-
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-
-    console.log(
-      `[${req.method}] ${req.originalUrl} - ${res.statusCode} - ${duration}ms`,
-    );
-  });
+  const logMessage = `${req.method} ${req.url}`;
+  writeLog(logMessage);
 
   next();
 };
---
+++ src/utils/file-logger.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+const logsDir = path.join(process.cwd(), "logs");
+const logFile = path.join(logsDir, "access.log");
+
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+export const writeLog = (message: string) => {
+  const log = `${new Date().toISOString()} ${message}\n`;
+
+  fs.appendFile(logFile, log, (err) => {
+    if (err) {
+      console.error("Log write error:", err);
+    }
+  });
+};
